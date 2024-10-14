c7c9b2093f02a7ad1c37b67a8ad744abec0d7f22
VGSProg <gitprogvov@gmail.com>
2024-10-14 10:39:18 +0300

    Add russian language

---
+++ lang/ru/block_users_aplus.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Users_apluss russian language translation
+ *
+ * @package    block_users_aplus
+ * @copyright  2024 
+ * @license 
+ */
+
+$string['pluginname'] = 'Users A+';
+$string['user_info_grades_no_list'] = 'список оценок пуст';
+
+$string['admin_setting_demo_users'] = 'Загрузить демо-пользователей';
+$string['admin_setting_demo_users_desc'] = 'Загружает список демо-пользователей для тестирования блока.';
